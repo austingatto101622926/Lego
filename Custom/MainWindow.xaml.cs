e002a802ea70c1611672d3f626f9ff2a19372d71
101622926 <101622926@TB210-010>
2018-02-22 11:41:14 +1100

    Add project files.

---
+++ Custom/MainWindow.xaml.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Lego.Ev3.Core;
+using Lego.Ev3.Desktop;
+
+namespace Custom
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Brick brick;
+        enum State { Foward, Back, Stopped, Unconnected };
+        State state = State.Unconnected;
+
+        static int turnSpeed = 40;
+        static int normalSpeed = 80;
+
+        int rightSpeed = normalSpeed;
+        int leftSpeed = normalSpeed;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void KeyPress_Handler(object sender, KeyEventArgs e)
+        {
+            if (Keyboard.IsKeyDown(Key.Enter))
+            {
+                Output.Text = "ENTER";
+                brick = new Brick(new UsbCommunication());
+                await brick.ConnectAsync();
+                await brick.DirectCommand.PlayToneAsync(40, 2, 500);
+                state = State.Stopped;
+            }
+            if (Keyboard.IsKeyDown(Key.W) && state == State.Stopped)
+            {
+                Output.Text = "UP";
+                brick.BatchCommand.TurnMotorAtSpeed(OutputPort.A, leftSpeed);
+                brick.BatchCommand.TurnMotorAtSpeed(OutputPort.D, rightSpeed);
+                brick.BatchCommand.StartMotor(OutputPort.A);
+                brick.BatchCommand.StartMotor(OutputPort.D);
+                await brick.BatchCommand.SendCommandAsync();
+                state = State.Foward;
+            }
+            if (Keyboard.IsKeyDown(Key.S) && state == State.Stopped)
+            {
+                Output.Text = "UP";
+                brick.BatchCommand.TurnMotorAtSpeed(OutputPort.A, -leftSpeed);
+                brick.BatchCommand.TurnMotorAtSpeed(OutputPort.D, -rightSpeed);
+                brick.BatchCommand.StartMotor(OutputPort.A);
+                brick.BatchCommand.StartMotor(OutputPort.D);
+                await brick.BatchCommand.SendCommandAsync();
+                state = State.Back;
+            }
+            if (Keyboard.IsKeyDown(Key.Space) && (state == State.Foward || state == State.Back))
+            {
+                Output.Text = "SPACE";
+                brick.BatchCommand.StopMotor(OutputPort.A, true);
+                brick.BatchCommand.StopMotor(OutputPort.D, true);
+                await brick.BatchCommand.SendCommandAsync();
+                state = State.Stopped;
+            }
+            if (Keyboard.IsKeyDown(Key.A))
+            {
+                Output.Text = "LEFT";
+                leftSpeed = turnSpeed;
+            }
+            if (Keyboard.IsKeyDown(Key.D))
+            {
+                Output.Text = "RIGHT";
+                rightSpeed = turnSpeed;
+            }
+            if (Keyboard.IsKeyDown(Key.Q))
+            {
+                Output.Text = "Q";
+                rightSpeed = normalSpeed;
+                leftSpeed = -normalSpeed;
+            }
+            if (Keyboard.IsKeyDown(Key.E))
+            {
+                Output.Text = "E";
+                leftSpeed = normalSpeed;
+                rightSpeed = -normalSpeed;
+            }
+        }
+
+        private async void KeyUp_Handler(object sender, KeyEventArgs e)
+        {
+            if ((e.Key == Key.W || e.Key == Key.S) && (state == State.Foward || state == State.Back))
+            {
+                Output.Text = "KEY UP";
+                brick.BatchCommand.StopMotor(OutputPort.A, false);
+                brick.BatchCommand.StopMotor(OutputPort.D, false);
+                await brick.BatchCommand.SendCommandAsync();
+                state = State.Stopped;
+            }
+            if (e.Key == Key.A)
+            {
+                Output.Text = "LEFT UP";
+                leftSpeed = normalSpeed;
+            }
+            if (e.Key == Key.D)
+            {
+                Output.Text = "RIGHT UP";
+                rightSpeed = normalSpeed;
+            }
+            if (e.Key == Key.Q)
+            {
+                Output.Text = "Q UP";
+                rightSpeed = normalSpeed;
+                leftSpeed = normalSpeed;
+            }
+            if (e.Key == Key.E)
+            {
+                Output.Text = "E UP";
+                rightSpeed = normalSpeed;
+                leftSpeed = normalSpeed;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# LEGO MINDSTORMS EV3 API for .NET
+
+A .NET API for the LEGO MINDSTORMS EV3 brick usable from desktop, Windows Phone and WinRT.  Control your LEGO MINDSTORMS EV3 kit! With this API, you can connect, control and read sensor data from your LEGO EV3 brick over Bluetooth, WiFi, or USB. 
+
+This project provides libraries that are usable from the Windows desktop, Windows Phone 8, and WinRT (via .NET, WinJS and C++), along with full source code.
+
+Important links:
+* [Downloads](https://github.com/BrianPeek/legoev3/releases)
+* [Docs](https://github.com/BrianPeek/legoev3/wiki)
+* [Getting Started video](https://channel9.msdn.com/posts/LEGO-Mindstorms-EV3-API)
